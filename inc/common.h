fffedb964dacbcb1c5d954404491b545b2d48c5e
Yagigami <yagigami.goatgod@gmail.com>
2020-10-18 17:20:56 +0200

    change buffer representations

--- inc/common.h
+++ inc/common.h
@@ -22,17 +22,61 @@
 
 typedef ptrdiff_t len_t;
 
+// TODO: use small_buf of char instead
 typedef struct identifier {
 	int8_t len;
 	char str[];
 } identifier;
 
-typedef const identifier *ident_t;
+// typedef const identifier *ident_t;
 
 typedef struct stream {
 	len_t len;
 	char *buf;
 } stream;
+
+// TODO: replace all those with small_buf
+typedef struct stretchy_buf {
+	len_t len: sizeof (len_t) * CHAR_BIT - 8;
+	len_t log2_cap: 8;
+	void *mem;
+} stretchy_buf;
+
+// useful for strings, where the pointer isn't aligned
+typedef uintptr_t fixed_buf;
+	// bits [48..64[: len
+	// bits [ 0..48[: ptr
+
+len_t fb_len(fixed_buf b);
+void *fb_mem(fixed_buf b);
+void fb_set_len(fixed_buf *b, len_t l);
+void fb_set_mem(fixed_buf *b, void *m);
+fixed_buf fb_set(len_t len, void *mem);
+
+len_t sb_cap(stretchy_buf buf);
+void *sb_add(stretchy_buf *buf, void *obj, len_t objsz);
+void sb_resize(stretchy_buf *buf, len_t n);
+void sb_fini(stretchy_buf *buf);
+void sb_shrink_into(fixed_buf *restrict dst, const stretchy_buf *restrict src, len_t objsz);
+
+identifier *strdup_range(const char *start, const char *end);
+identifier *strdup_len(const char *start, len_t len);
+
+typedef uintptr_t small_buf;
+	// bit 64: reserved, 0 for now
+	// bits [48..64[: len
+	// bits [ 4..48[: mem
+	// bits [ 0.. 4[: log2_cap
+
+len_t sm_len(small_buf b);
+len_t sm_cap(small_buf b);
+void *sm_mem(small_buf b);
+void sm_set_mem(small_buf *b, void *mem);
+void *sm_add(small_buf *b, void *obj, len_t objsz);
+void *sm_resize(small_buf *b, len_t objsz);
+void *sm_shrink_into(small_buf *restrict dst, small_buf src, len_t objsz);
+
+typedef small_buf ident_t;
 
 #ifdef  __cplusplus
 }
--- src/token.c
+++ src/token.c
@@ -26,33 +26,22 @@ static ptrmap keywords;
 hash_func identifier_hash;
 hash_t identifier_hash(key_t k)
 {
-	identifier *id = k;
+	ident_t id = (ident_t) k;
 	hash_t h = 0;
-	for (int8_t i = 0; i < id->len; i++)
-		h += id->str[i] * (i + 1);
+	const char *restrict str = fb_mem(id);
+	for (len_t i = 0, len = sm_len(id); i < len; i++)
+		h += str[i] * (i + 1);
 	return h;
 }
 
 cmp_func identifier_cmp;
 int identifier_cmp(key_t k1, key_t k2)
 {
-	ident_t i1 = k1, i2 = k2;
-	int diff = i1->len - i2->len;
+	ident_t i1 = (ident_t) k1, i2 = (ident_t) k2;
+	len_t l1 = fb_len(i1), l2 = fb_len(i2);
+	int diff = l1 - l2;
 	if (diff) return diff;
-	return strncmp(i1->str, i2->str, i1->len);
-}
-
-__attribute__((constructor))
-void keywords_init(void)
-{
-	pmap_reserve(&keywords, 7);
-#define X(w) static identifier i ## w = { sizeof # w - 1, # w };
-	KEYWORDS()
-#undef X
-
-#define X(w) kw_ ## w = *pmap_push(&keywords, &i ## w, identifier_hash);
-	KEYWORDS()
-#undef X
+	return strncmp(fb_mem(i1), fb_mem(i2), l1);
 }
 
 void lexer_init(lexer *l, stream s)
@@ -65,41 +54,27 @@ void lexer_init(lexer *l, stream s)
 	memset(&l->ids, 0, sizeof l->ids);
 }
 
-bool is_keyword(ident_t id)
-{
-	return (bool) pmap_find(&keywords, (key_t) id, identifier_hash, identifier_cmp);
-}
-
 void lexer_next(lexer *l)
 {
 space:
 	;
 	char *start = l->str;
+	ident_t name, *id;
 	switch (*l->str) {
-		ident_t *id;
-		identifier *name;
+	case '\0':
+		l->tok.kind = TK_EOF;
+		return;
 	case ' ': case '\t': case '\n': case '\f': case '\v':
 		while (isspace(*l->str)) l->str++;
 		goto space;
-	case '\0':
-		l->tok.kind = TK_EOF;
-		break;
 	case 'a' ... 'z': case 'A' ... 'Z': case '_':
 		while (isalpha(*l->str) || *l->str == '_') l->str++;
-		name = xmalloc(sizeof *name + (l->str - start));
-		name->len = l->str - start;
-		memcpy(name->str, start, name->len);
-		id = (ident_t *) pmap_find(&keywords, name, identifier_hash, identifier_cmp);
-		if (id) {
-			l->tok.kind = TK_KW;
-			l->tok.tid = *id;
-			xfree(name);
-		} else {
-			l->tok.kind = TK_NAME;
-			// may already be in the map
-			l->tok.tid = *pmap_push(&l->ids, name, identifier_hash);
-		}
-		break;
+		name = fb_set(l->str - start, start);
+		id = (ident_t *) pmap_find(&l->ids, (void *) name, identifier_hash, identifier_cmp);
+		if (!id) id = (ident_t *) pmap_push(&l->ids, (void *) name, identifier_hash);
+		l->tok.tid = *id;
+		l->tok.kind = TK_NAME;
+		return;
 	case '0' ... '9':
 		l->tok.kind = TK_INT;
 		l->tok.tint = *l->str++ - '0';
@@ -113,7 +88,7 @@ space:
 			goto space;
 		}
 		break;
-#define CASE1(x) case x: l->tok.kind = *l->str++; break
+#define CASE1(x) case x: l->tok.kind = *l->str; break
 	CASE1('(');
 	CASE1(')');
 	CASE1(':');
@@ -121,6 +96,7 @@ space:
 	CASE1('=');
 	CASE1('{');
 	CASE1('}');
+	CASE1(',');
 #undef CASE1
 	default:
 		fprintf(stderr, "unknown character `%c` (%d)\n", *l->str, *l->str);
@@ -132,9 +108,6 @@ space:
 void token_print(FILE *f, token t)
 {
 	switch (t.kind) {
-	case TK_NONE:
-		fprintf(f, "<unknown>\n");
-		break;
 	case TK_ASCII:
 	case TK_END:
 		fprintf(f, "<this shouldn't happpen\n");
@@ -144,9 +117,9 @@ void token_print(FILE *f, token t)
 		break;
 	case TK_NAME:
 		fprintf(f, "id[%d](%.*s)\n",
-				t.tid->len,
-				t.tid->len,
-				t.tid->str);
+				(int) fb_len(t.tid),
+				(int) fb_len(t.tid),
+				(char *) fb_mem(t.tid));
 		break;
 	case TK_STR:
 		fprintf(f, "str[%d](%.*s)\n",
@@ -154,27 +127,17 @@ void token_print(FILE *f, token t)
 				(int) (t.tstr[1] - t.tstr[0]),
 				t.tstr[0]);
 		break;
-	case TK_KW:
-		if (0);
-#define X(w) \
-		else if (t.tid == kw_ ## w) fprintf(f, "kw(" # w ")\n");
-		KEYWORDS()
-#undef X
-		else
-			fprintf(f, "kw(<unknown>)\n");
-		break;
 	case TK_EOF:
 		fprintf(f, "<EOF>\n");
+		break;
+	default:
+		fprintf(f, "%c\n", t.kind);
 		break;
 	}
 }
 
 void lexer_fini(lexer *l)
 {
-	uint8_t *meta = l->ids.mem;
-	key_t *buf = (key_t *) ((char *) l->ids.mem + (1 << l->ids.log2_cap));
-	for (len_t i = 0; i < (1 << l->ids.log2_cap); i++) {
-		if ((meta[i] & 0x80))
-			xfree(buf[i]);
-	}
+	xfree(l->ids.mem);
+	// we don't allocate for names so no need to free them
 }
